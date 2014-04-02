1c8ab0475e233ffd9af3d16119a39ad98271dd4f
Benjamin Doherty <bjd@bangpound.org>
2014-04-02 05:24:38 -0500

    use the listeners as pimple services.

--- LegacyPhpHttpKernelProvider.php
+++ LegacyPhpHttpKernelProvider.php
@@ -7,6 +7,8 @@ use Bangpound\LegacyPhp\EventListener\ShutdownListener;
 use Bangpound\LegacyPhp\HttpKernel;
 use Silex\Application;
 use Silex\ServiceProviderInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpFoundation\RequestMatcher;
 
 class LegacyPhpHttpKernelProvider implements ServiceProviderInterface
 {
@@ -20,9 +22,33 @@ class LegacyPhpHttpKernelProvider implements ServiceProviderInterface
      */
     public function register(Application $app)
     {
-        $app['kernel'] = $app->share(function ($c) {
-            return new HttpKernel($c['dispatcher'], $c['resolver'], $c['request_stack']);
-        });
+        $app['kernel'] = $app->share(
+            function ($c) {
+                return new HttpKernel($c['dispatcher'], $c['resolver'], $c['request_stack']);
+            }
+        );
+
+        $app['legacy.request_matcher'] = $app->share(
+            function () {
+                return new RequestMatcher();
+            }
+        );
+
+        $app['legacy.listener.output_buffer'] = $app->share(
+            function ($c) {
+                return new OutputBufferListener($c['legacy.request_matcher']);
+            }
+        );
+        $app['legacy.listener.shutdown'] = $app->share(
+            function ($c) {
+                return new ShutdownListener($c['legacy.request_matcher']);
+            }
+        );
+        $app['legacy.listener.header'] = $app->share(
+            function ($c) {
+                return new HeaderListener($c['legacy.request_matcher']);
+            }
+        );
     }
 
     /**
@@ -34,10 +60,10 @@ class LegacyPhpHttpKernelProvider implements ServiceProviderInterface
      */
     public function boot(Application $app)
     {
+        /** @var EventDispatcherInterface $dispatcher */
         $dispatcher = $app['dispatcher'];
-
-        $dispatcher->addSubscriber(new OutputBufferListener($app['legacy.request_matcher']));
-        $dispatcher->addSubscriber(new ShutdownListener($app['legacy.request_matcher']));
-        $dispatcher->addSubscriber(new HeaderListener($app['legacy.request_matcher']));
+        $dispatcher->addSubscriber($app['legacy.listener.output_buffer']);
+        $dispatcher->addSubscriber($app['legacy.listener.shutdown']);
+        $dispatcher->addSubscriber($app['legacy.listener.header']);
     }
 }
